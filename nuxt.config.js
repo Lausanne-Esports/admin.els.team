98661e2e273bf8ce0e1b233d2da38572088e5ab5
Aperrix <aperrix@gmail.com>
2019-02-24 13:35:34 +0100

    chore(nuxt): replace title
    Replace "title" by "titleTemplate" for more flexibility : 
    
    ```js
    head: {
        titleTemplate: 'PAGE_NAME | ORG_NAME',
      },
    ```

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Admin | Lausanne eSports',
+    titleTemplate: '%s | ' process.env.ORG_NAME,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
