6f2003a33bb8e4546dcc526d63dce9239e5cfd2e
Romain Lanz <romain.lanz+github@pm.me>
2019-11-06 01:36:01 +0100

    style(TeamMemberListItem): change colour border

--- components/Team/TeamMemberListItem.vue
+++ components/Team/TeamMemberListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <tr class="h-16 border-b border-primary-lightest">
+  <tr class="h-16 border-b border-neutral-200">
     <td>{{ fullname }}</td>
     <td>{{ member.pivot.role }}</td>
     <td>
@@ -18,7 +18,7 @@
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/></svg>
         </button>
-      </div> -->
+      </div>-->
     </td>
     <td>
       <div class="flex item-centers">
@@ -26,7 +26,18 @@
           class="inline-flex text-s-red-600 shadow items-center justify-center hover:bg-neutral-100 transition rounded h-10 w-10 text-neutral-700"
           @click="deleteMember"
         >
-          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8H3a1 1 0 1 1 0-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1z"/></svg>
+          <svg
+            class="fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path
+              class="heroicon-ui"
+              d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8H3a1 1 0 1 1 0-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1z"
+            />
+          </svg>
         </button>
       </div>
     </td>
@@ -40,15 +51,18 @@ export default {
   props: ['first', 'last', 'member', 'team'],
 
   computed: {
-    fullname () {
-      return `${this.member.firstname || ''} "${this.member.nickname}" ${this.member.lastname || ''}`.trim()
+    fullname() {
+      return `${this.member.firstname || ''} "${this.member.nickname}" ${this
+        .member.lastname || ''}`.trim()
     },
   },
 
   methods: {
-    async deleteMember () {
+    async deleteMember() {
       try {
-        await this.$axios.$delete(`admin/teams/${this.team.id}/members/${this.member.id}`)
+        await this.$axios.$delete(
+          `admin/teams/${this.team.id}/members/${this.member.id}`
+        )
         this.$toast.success("Membre supprimé de l'équipe !")
         this.$emit('submit')
       } catch (e) {
@@ -56,13 +70,17 @@ export default {
       }
     },
 
-    async up () {
-      await this.$axios.$post(`admin/teams/${this.team.id}/members/${this.member.id}/up`)
+    async up() {
+      await this.$axios.$post(
+        `admin/teams/${this.team.id}/members/${this.member.id}/up`
+      )
       this.$emit('submit')
     },
 
-    async down () {
-      await this.$axios.$post(`admin/teams/${this.team.id}/members/${this.member.id}/down`)
+    async down() {
+      await this.$axios.$post(
+        `admin/teams/${this.team.id}/members/${this.member.id}/down`
+      )
       this.$emit('submit')
     },
   },
