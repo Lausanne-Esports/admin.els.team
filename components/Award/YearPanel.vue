dcf0a2cf3d1f2c753c82872b8ef39f4c2e84e6af
Romain Lanz <romain.lanz+github@pm.me>
2019-12-19 23:42:06 +0100

    feat(Award): let use order the category

--- components/Award/YearPanel.vue
+++ components/Award/YearPanel.vue
@@ -1,17 +1,37 @@
 <template>
   <Panel :header="year" :key="year">
     <template #tools>
-      <BaseSelect label="Category" name="category_id" v-model="filteredByCategory">
+      <BaseSelect
+        label="Category"
+        name="category_id"
+        v-model="filteredByCategory"
+      >
         <option value="0">All</option>
         <option
           :value="category.id"
           v-for="category in categories"
           :key="category.id"
-        >{{ category.name }}</option>
+          >{{ category.name }}</option
+        >
       </BaseSelect>
     </template>
 
     <div class="w-full">
+      <Draggable
+        class="flex mb-4"
+        v-model="localOrderByCategory"
+        tag="ul"
+        @update="computeCategoryOrder"
+      >
+        <li
+          class="bg-white border border-gray-300 shadow hover:shadow-md rounded cursor-pointer py-2 px-4 mr-4"
+          v-for="cat of localOrderByCategory"
+          :key="cat.id"
+        >
+          {{ getCategoryNameById(cat.category_id) }}
+        </li>
+      </Draggable>
+
       <table class="w-full border-collapse">
         <thead>
           <tr class="text-sm h-12 text-left uppercase text-gray-500">
@@ -23,7 +43,11 @@
           </tr>
         </thead>
         <Draggable v-model="localResults" tag="tbody" @update="computeNewOrder">
-          <ListItem v-for="award in awardByCategory" :key="award.id" :award="award" />
+          <ListItem
+            v-for="award in awardByCategory"
+            :key="award.id"
+            :award="award"
+          />
         </Draggable>
       </table>
     </div>
@@ -45,6 +69,10 @@ export default {
       type: Array,
       required: true,
     },
+    categoryOrder: {
+      type: Array,
+      required: true,
+    },
     results: {
       type: Array,
       required: true,
@@ -53,6 +81,7 @@ export default {
 
   data: () => ({
     localResults: [],
+    localOrderByCategory: [],
     filteredByCategory: 0,
   }),
 
@@ -65,6 +94,10 @@ export default {
   created() {
     this.localResults = [
       ...this.results.slice().sort((a, b) => a.order - b.order),
+    ]
+
+    this.localOrderByCategory = [
+      ...this.categoryOrder.slice().sort((a, b) => a.order - b.order),
     ]
   },
 
@@ -81,6 +114,10 @@ export default {
   },
 
   methods: {
+    getCategoryNameById(id) {
+      return this.categories.find(c => c.id === id).name
+    },
+
     computeNewOrder() {
       this.localResults = this.localResults.map((award, i) => ({
         ...award,
@@ -88,6 +125,17 @@ export default {
       }))
 
       this.$emit('updateOrder', this.localResults)
+    },
+
+    computeCategoryOrder() {
+      this.localOrderByCategory = this.localOrderByCategory.map(
+        (category, i) => ({
+          ...category,
+          order: i + 1,
+        })
+      )
+
+      this.$emit('updateCategoryOrder', this.localOrderByCategory)
     },
   },
 }
